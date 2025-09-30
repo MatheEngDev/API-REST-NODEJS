3630eb7e3859ccfffe659a6f60462977099dcc80
matheusillidan <mathe147852@gmail.com>
2025-09-29 22:53:57 -0300

    criação de rotas

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,12 +1,31 @@
 import { FastifyInstance } from "fastify";
 import { knex } from "../database";
+import { z } from "zod";
+import { randomUUID } from "node:crypto";
+import tr from "zod/v4/locales/tr.js";
 
-export async function transactionsRoutes(app: FastifyInstance){
-    app.get("/hello", async () => {
-      const transactions = await knex("transactions")
-        .where("amount", 1000)
-        .select("*");
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.post("/", async (request, reply) => {
+    // {title, amount, type: credit ou debit}
 
-      return transactions;
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(["credit", "debit"]),
     });
+
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body
+    );
+
+   await knex("transactions")
+    .insert({
+
+    id: randomUUID(),
+    title,
+    amount: type === "credit" ? amount : amount * -1,
+    });
+
+    return reply.status(201).send()
+  });
 }
