9774b15b4dac14049bc85f98eff69e4f96e618fb
matheusillidan <mathe147852@gmail.com>
2025-10-01 23:29:25 -0300

    criação de types para knex

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,14 @@
+//esLint-disable-next-line
+import { knex } from "knex";
+
+declare module "knex/types/tables" {
+  export interface Tables {
+    transactions: {
+      id: string;
+      title: string
+      amount: number
+      created_at: Date
+      session_id: string
+    };
+  }
+}
