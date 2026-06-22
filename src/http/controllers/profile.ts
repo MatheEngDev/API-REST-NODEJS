ed8b6c01dbb6444f078901fd16f17de12d50e07d
matheusillidan <mathe147852@gmail.com>
2026-06-21 22:39:49 -0300

    Controllers & Testes E2E - Controller de perfil

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,9 +1,18 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
+import { makeGetUserProfileUseCase } from "../../use-cases/factories/make-get-user-profile-use-case";
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify();
 
-console.log(request.user.sub);
+  const getUserProfile = makeGetUserProfileUseCase();
 
-  return reply.status(200).send();
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  });
+
+  return reply.status(200).send({
+    user: {
+      ...user,
+      passwordHash: undefined,
+    },
+  });
 }
