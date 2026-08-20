bfb0029828aaa85254aac5eebaa680140afc1b7c
matheusillidan <mathe147852@gmail.com>
2026-08-19 21:24:55 -0300

    Controllers & Testes E2E - Outros controllers da academia

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,29 @@
+import z from "zod";
+import type { FastifyReply, FastifyRequest } from "fastify";
+import { makeFetchNearbyGymsUseCase } from "@/use-cases/factories/make-fetch-nearby-gyms-use-case";
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+  const nearbyGymsQuerySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) >= 90;
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) >= 180;
+    }),
+  });
+
+  // SOLID - D - principio da inversão de dependência
+
+  const { latitude, longitude } = nearbyGymsQuerySchema.parse(request.body);
+
+  const fetchNearbyGymsUseCase = makeFetchNearbyGymsUseCase();
+
+  const { gyms } = await fetchNearbyGymsUseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  });
+
+  return reply.status(200).send({
+    gyms,
+  });
+}
---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,25 @@
+import z from "zod";
+import type { FastifyReply, FastifyRequest } from "fastify";
+import { makeSearchGymsUseCase } from "@/use-cases/factories/make-search-gyms-use-case";
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymsQuerySchema = z.object({
+    q: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  });
+
+  // SOLID - D - principio da inversão de dependência
+
+  const { q, page } = searchGymsQuerySchema.parse(request.body);
+
+  const searchGymsUseCase = makeSearchGymsUseCase();
+
+  const { gyms } = await searchGymsUseCase.execute({
+    query: q,
+    page,
+  });
+
+  return reply.status(200).send({
+    gyms,
+  });
+}
